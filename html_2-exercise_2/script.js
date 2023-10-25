738116bac909234c5298649bb9670dc7f5449373
afateel2 <119856838+afateel2@users.noreply.github.com>
2023-10-25 19:17:44 +0300

    JS-3_Exercise-1

--- html_2-exercise_2/script.js
+++ html_2-exercise_2/script.js
@@ -1,43 +1,37 @@
 images = [
     {
-        src:"assets/icons8-whatsapp-50.png",
-        href:"https://wa.me/966570708311"
- 
-    },
-    {
-        
-    src:"assets/icons8-linkedin-50.png",
-    href:"https://www.linkedin.com/in/abdullah-alfateel-2755a3294/"
-    
+        src: "assets/icons8-whatsapp-50.png",
+        href: "https://wa.me/966570708311"
+
     },
     {
 
-    src:"assets/icons8-github-50.png",
-    href:"https://github.com/afateel2"
-    
+        src: "assets/icons8-linkedin-50.png",
+        href: "https://www.linkedin.com/in/abdullah-alfateel-2755a3294/"
+
     },
     {
 
-  
-    src:"assets/icons8-instagram-50.png",
-    href:"https://www.instagram.com/afateel2"
-  
+        src: "assets/icons8-github-50.png",
+        href: "https://github.com/afateel2"
+
     },
     {
 
-        src:"assets/icons8-twitterx-50.png",
-        href:"https://twitter.com/A2F_FL"
-        
+
+        src: "assets/icons8-instagram-50.png",
+        href: "https://www.instagram.com/afateel2"
+
     },
-    
+    {
+
+        src: "assets/icons8-twitterx-50.png",
+        href: "https://twitter.com/A2F_FL"
+
+    },
+
 ]
-/*
-        <a href="https://wa.me/966570708311"><img class="icons" src="assets/icons8-whatsapp-50.png" alt=""> </a>
-        <a href="https://www.linkedin.com/in/abdullah-alfateel-2755a3294/"> <img class="icons" src="assets/icons8-linkedin-50.png" alt=""> </a>
-        <a href="https://github.com/afateel2"> <img class="icons" src="assets/icons8-github-50.png" alt=""> </a>
-        <a href="https://www.instagram.com/afateel2"> <img class="icons" src="assets/icons8-instagram-50.png" alt=""> </a>
-        <a href="https://twitter.com/A2F_FL"> <img class="icons" src="assets/icons8-twitterx-50.png" alt=""> </a>
-*/
+
 // JS 2 Exercise 1
 let div = document.createElement('div')
 div.classList.add("iconBar")
@@ -51,3 +45,30 @@ images.forEach(element => {
     div.appendChild(anchor)
 });
 document.querySelector("#iconsDiv").appendChild(div)
+
+function loadImage() {
+
+    fetch("h")
+        .then(response => response.json())
+        .then(data => {
+
+            return data.urls.raw
+
+        })
+
+}
+
+// JS 3 Exercise 1
+imgs = document.querySelectorAll('#fetPics img');
+imgs.forEach(imag => {
+    fetch("https://api.unsplash.com/photos/random?query=space&per_page=8&client_id=D5P-LSTm4K7vmyPVnw-Lmj9SpjiVichInWGM5vY7JQQ")
+        .then(response => response.json())
+        .then(data => {
+
+            imag.src = data.urls.raw
+
+        })
+
+})
+
+
