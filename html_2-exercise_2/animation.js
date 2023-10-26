8c620306b8bf5908326a699828de1575b6486ca9
afateel2 <119856838+afateel2@users.noreply.github.com>
2023-10-26 19:09:42 +0300

    JS 4 - Exercise 3

--- html_2-exercise_2/animation.js
+++ html_2-exercise_2/animation.js
@@ -15,96 +15,100 @@ renderer.setSize(container.clientWidth, 900);
 
 //JS 4 Exercise 1 & 2->(Part 2)
 class TorusObject {
-    constructor(radius, tube, radialSegments, tubularSegments, color, metalness, roughness) {
-        this.geometry = new THREE.TorusGeometry(radius, tube, radialSegments, tubularSegments);
-        this.material = new THREE.MeshStandardMaterial({ color: color, metalness: metalness, roughness: roughness });
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-        this.mesh.position.z = -100;
-        this.light = new THREE.DirectionalLight(0xffffff);
-        this.light.castShadow = true;
-        this.light.target = this.mesh;
-        scene.add(this.mesh);
-        scene.add(this.light)
-    }
-    
-    rotateX(amount) {
-        this.mesh.rotation.x += amount;
-    }
+  constructor(radius, tube, radialSegments, tubularSegments, color, metalness, roughness) {
+    this.geometry = new THREE.TorusGeometry(radius, tube, radialSegments, tubularSegments);
+    this.material = new THREE.MeshStandardMaterial({ color: color, metalness: metalness, roughness: roughness });
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.mesh.position.z = -100;
+    this.light = new THREE.DirectionalLight(0xffffff);
+    this.light.castShadow = true;
+    this.light.target = this.mesh;
+    scene.add(this.mesh);
+    scene.add(this.light);
+  }
 
-    rotateY(amount) {
-        this.mesh.rotation.y += amount;
-    }
+  rotateX(amount) {
+    this.mesh.rotation.x += amount;
+  }
 
-    moveZ(amount) {
-        this.mesh.position.z += amount;
-    }
+  rotateY(amount) {
+    this.mesh.rotation.y += amount;
+  }
 }
 
+//JS 4 Exercise 3 & 1
+//Array of objects
+const torusData = [
+  { radius: 40, tube: 2, radialSegments: 14, tubularSegments: 100, color: 0xfff000, metalness: 2, roughness: 1 },
+  { radius: 32, tube: 2, radialSegments: 14, tubularSegments: 100, color: 0x000fff, metalness: 2, roughness: 1 },
+  { radius: 15, tube: 2, radialSegments: 14, tubularSegments: 100, color: 0xff00ff, metalness: 2, roughness: 1 },
+  { radius: 25, tube: 2, radialSegments: 14, tubularSegments: 100, color: 0x00A36C, metalness: 2, roughness: 1 }
+];
 
-const torus1 = new TorusObject(40, 2, 14, 100, 0xfff000, 2, 1);
-const torus2 = new TorusObject(32, 2, 14, 100, 0x000fff, 2, 1);
-const torus3 = new TorusObject(15, 2, 14, 100, 0xff00ff, 2, 1);
-const torus4 = new TorusObject(25, 2, 14, 100, 0x00A36C, 2, 1);
-const obj = new THREE.SphereGeometry(10,32,16);
-const mat = new THREE.MeshStandardMaterial({ color: 0xffffff, metalness: 0, roughness: 1 });
-const sh = new THREE.Mesh(obj,mat);
-scene.add(sh);
-sh.position.z = -100;
-const Li = new THREE.DirectionalLight(0x00000);
-Li.castShadow = true;
-Li.target = sh;
-scene.add(Li);
+//Utilizing map functions on an array.
+const torusObjects = torusData.map(data => new TorusObject(
+  data.radius,
+  data.tube,
+  data.radialSegments,
+  data.tubularSegments,
+  data.color,
+  data.metalness,
+  data.roughness
+));
 
-//l
-const am = new THREE.Light(0x11111, 1);
-scene.add(am);
+const sphereGeometry = new THREE.SphereGeometry(10, 32, 16);
+const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff, metalness: 0, roughness: 1 });
+const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphereMesh);
+sphereMesh.position.z = -100;
+
+const sphereLight = new THREE.DirectionalLight(0x00000);
+sphereLight.castShadow = true;
+sphereLight.target = sphereMesh;
+scene.add(sphereLight);
+
+//Backgroind color
 scene.background = new THREE.Color(0x505050);
 
-var forward = false;
-var counter = 0;
-let animation;
 const screenSaver = () => {
-    //x-axis
-    torus1.rotateX(0.035);
-    torus2.rotateX(-0.025);
-    torus3.rotateX(-0.015);
-    torus4.rotateX(0.020);
-    sh.rotation.x -= 0.005;  //sphere
+  torusObjects.forEach((torus, index) => {
+    torus.rotateX(index === 0 ? 0.035 : index === 1 ? -0.025 : index === 2 ? -0.015 : 0.020);
+    torus.rotateY(index === 0 ? 0.0015 : index === 1 ? -0.0025 : index === 2 ? -0.0035 : 0.0030);
+  });
 
+  sphereMesh.rotation.x -= 0.005;
+  sphereMesh.rotation.y += 0.0005;
+  sphereMesh.position.z += forward ? 2 : -2;
 
-    //y-axis
-    sh.rotation.y += 0.0005;//sphere
-    torus1.rotateY(0.0015);
-    torus2.rotateY(-0.0025);
-    torus3.rotateY(-0.0035);
-    torus4.rotateY(0.0030);
+  counter += 1;
+  if (counter === 3) {
+    forward = !forward;
+    counter = 0;
+  }
 
-    sh.position.z += (forward ? 2 : -2);
-    counter += 1;
-    if (counter == 3) {
-        forward = !forward;
-        counter = 0;
-    }
+  renderer.render(scene, camera);
+  requestAnimationFrame(screenSaver);
+};
 
-    renderer.render(scene, camera);
-    requestAnimationFrame(screenSaver);
-   
-}
+let forward = false;
+let counter = 0;
 let timer;
+
 const displayScreenSaver = () => {
-    document.getElementById('content').style.visibility = 'hidden';
-    container.style.visibility = 'visible';
-    screenSaver();
-  };
-  
-  const resetTimer = () => {
-    cancelAnimationFrame(screenSaver);
-    document.getElementById('content').style.visibility = 'visible';
-    container.style.visibility = 'hidden';
-  
-    clearTimeout(timer);
-    timer = setTimeout(displayScreenSaver, 3000);
-  };
+  document.getElementById('content').style.visibility = 'hidden';
+  container.style.visibility = 'visible';
+  screenSaver();
+};
+
+const resetTimer = () => {
+  cancelAnimationFrame(screenSaver);
+  document.getElementById('content').style.visibility = 'visible';
+  container.style.visibility = 'hidden';
+
+  clearTimeout(timer);
+  timer = setTimeout(displayScreenSaver, 3000);
+};
+
 document.addEventListener('mousemove', resetTimer);
 document.addEventListener('keydown', resetTimer);
 document.addEventListener('touchstart', resetTimer);
