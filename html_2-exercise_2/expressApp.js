359b0f949ce684dfb2527c454e3c565f0e905fe0
afateel2 <119856838+afateel2@users.noreply.github.com>
2023-11-27 21:46:21 +0300

    BE-3 Exercise 2

---
+++ html_2-exercise_2/expressApp.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const path = require('path')
+// const proposal = require('./prop')
+app.use(express.static(path.join(__dirname,"")))
+
+app.get('/', (request,response) =>{
+    const fileP = path.join(__dirname,"Home.html")
+    response.status(200).sendFile(fileP)
+})
+
+app.get('/proposal', (request,response) =>{
+    const fileP = path.join(__dirname,"prop.html")
+    response.status(200).sendFile(fileP)
+})
+
+app.all('*', (request,response) =>{
+    response.status(200).send("<h1>404 <br> <br>The page you are looking for does not exist!</h1>")
+})
+
+
+
+app.listen(3000, () =>{ 
+    console.log(
+        "Server is running on port 3000"
+    )
+})
